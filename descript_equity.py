f0974c5c755bb77c3fe1338cfad30fa7ca81a792
aefremis <aefremis@hotmail.com>
2022-12-12 17:24:09 +0200

    better swasonality - still manual

--- descript_equity.py
+++ descript_equity.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 plt.interactive(False)
 plt.style.use('ggplot')
 import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 # select asset
 asset_series = asset(symbol='VUSA.AS',
@@ -27,12 +28,46 @@ plt.ylabel('Value')
 plt.xticks(rotation = 45)
 plt.show()
 
+# build covariates
+
 
 # plot seasonality and trend plots
+#auto detect seasonality based on covariates lm
+
+decompose_result_mult = seasonal_decompose(raw[['close']], period=62)
+
+fig, axs = plt.subplots(ncols=2, nrows=2)
+ax1, ax2, ax3, ax4 = axs.flat
+fig.suptitle('Seasonality & Trend')
+
+ax1.plot(decompose_result_mult.seasonal,color = 'navy')
+ax1.set_ylabel('Seasonal Index')
+ax1.tick_params(labelrotation=45)
+
+ax2.plot(decompose_result_mult.trend, color = 'navy')
+ax2.set_xlabel('Date')
+ax2.set_ylabel('Trend line')
+ax2.tick_params(labelrotation=45)
+
+ax3.plot(decompose_result_mult.resid, color = 'navy')
+ax3.set_xlabel('Date')
+ax3.set_ylabel('Residuals')
+ax3.tick_params(labelrotation=45)
+
+ax4.plot(decompose_result_mult.observed, color = 'navy')
+ax4.set_xlabel('Date')
+ax4.set_ylabel('Observed')
+ax4.tick_params(labelrotation=45)
+
+plt.show()
+
+#decompose_result_mult.plot().show()
 # plot candle stick
+
+
 # plot acf pacf
 # outlier analysis
-# build covariates
+
 # plot box plots per category
 
 
