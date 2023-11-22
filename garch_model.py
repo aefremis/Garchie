c78364367cf82f2b3828ec0f5615e8c4c8179c0c
aefremis <aefremis@hotmail.com>
2023-11-22 16:54:02 +0200

    make garch module parametric before turing it into a class

--- garch_model.py
+++ garch_model.py
@@ -8,16 +8,20 @@ import arch
 from fetch_equity import crypto, asset
 from itertools import product
 from statsmodels.stats.diagnostic import acorr_ljungbox
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from arch.__future__ import reindexing
 
 
 # select asset
-asset_series = asset(symbol='vusa.as',granularity='1d', start= '2023-01-01', end='2023-10-10')
+asset_series = asset(symbol='vusa.as',granularity='1d', start= '2023-01-01', end='2023-11-21')
 print(asset_series)
 raw = asset_series.fetch_asset()
+raw.reset_index(inplace=True)
 
 #raw.set_index('date',inplace=True)
 ts = raw['return'].copy()
-
+forecast_ahead = 7
+fixed_window = False
 
 # Calculate daily std of returns
 '''
@@ -41,14 +45,24 @@ def fit_evaluate_garch(p_order, q_order, o_order,mean_type, vol_type ,dist_type 
             "bic": results.bic,
             "loglikelihood": results.loglikelihood}
 
+## Backtesting with MAE, MSE
+def evaluate(observation, forecast):
+    # Call sklearn function to calculate MAE
+    mae = mean_absolute_error(observation, forecast)
+    print('Mean Absolute Error (MAE): {:.3g}'.format(mae))
+    # Call sklearn function to calculate MSE
+    mse = mean_squared_error(observation, forecast)
+    print('Mean Squared Error (MSE): {:.3g}'.format(mse))
+    return mae, mse
+
 # Define the parameter grid
 p_values = range(1,4)
 q_values = range(3)
 o_values = range(1)
-mean_type_values = ['constant','zero','AR']
-distribution_values = ['normal','skewt','t']
-vol_values = ['GARCH','EGARCH']
-parameter_grid = list(product(p_values,q_values,o_values,
+mean_type_values = ['constant', 'zero', 'AR']
+distribution_values = ['normal', 'skewt', 't']
+vol_values = ['GARCH', 'EGARCH']
+parameter_grid = list(product(p_values, q_values, o_values,
                               mean_type_values,
                               vol_values,
                               distribution_values))
@@ -87,83 +101,57 @@ results_df.drop(['rank_aic', 'rank_sbc', 'rank_loglik','rank_total'], axis=1, in
 best_p, best_q, best_o, best_mean, best_vol, best_dist = parameter_grid.iloc[best_pos,:] # here check for skiped models
 
 # Fit the best model to the full data
-best_p, best_q, best_o = int(best_p),int(best_q),int(best_o)
+best_p, best_q, best_o = int(best_p), int(best_q), int(best_o)
 best_model = arch.arch_model(ts, vol=best_vol, p=best_p, q=best_q, o = best_o,
                              mean=best_mean, dist=best_dist)
 best_results = best_model.fit(disp='off')
 
 # Visualize results
-best_results.plot()
-
-# #Fixed rolling window forecast
-ts.index = raw.index
-index = ts.index
-start_loc = 0
-end_loc = np.where(index >= '2023-08-30')[0].min()
-forecasts = {}
-from arch.__future__ import reindexing
-for i in range(len(ts) - end_loc):
-    sys.stdout.write('o')
-    sys.stdout.flush()
-    res = best_model.fit(first_obs=start_loc + i, last_obs=i + end_loc, disp='off')
-    temp = res.forecast(horizon=1,start=end_loc).variance
-    fcast = temp.iloc[i]
-    forecasts[fcast.name] = fcast
-print(' Done!')
-variance_fixedwin = pd.DataFrame(forecasts).T
+# best_results.plot().show()
 
 
-## Expanding window forecast
-ts.index = raw.index
-index = ts.index
-start_loc = 0
-end_loc = np.where(index >= '2023-08-30')[0].min()
-forecasts = {}
-for i in range(len(ts) - end_loc):
-    sys.stdout.write('-')
-    sys.stdout.flush()
-    res = best_model.fit(first_obs = start_loc, last_obs = i + end_loc, disp = 'off')
-    temp = res.forecast(horizon=1,start=end_loc).variance
-    fcast = temp.iloc[i]
-    forecasts[fcast.name] = fcast
-print(' Done!')
-variance_expandwin = pd.DataFrame(forecasts).T
-## Compare forecast results
+if fixed_window:
+    ts.index = raw.date
+    index = ts.index
+    start_loc = 0
+    end_loc = len(index) - forecast_ahead
+    forecasts = {}
+    for i in range(len(ts) - end_loc):
+        sys.stdout.write('o')
+        sys.stdout.flush()
+        res = best_model.fit(first_obs=start_loc + i, last_obs=i + end_loc, disp='off')
+        temp = res.forecast(horizon=1, start=end_loc).variance
+        fcast = temp.iloc[i]
+        forecasts[fcast.name] = fcast
+    print(' Done!')
+    variance_holdout = pd.DataFrame(forecasts).T
+    variance_holdout = pd.Series(variance_holdout['h.1'])
+else:
+    ts.index = raw.date
+    index = ts.index
+    start_loc = 0
+    end_loc = len(index) - forecast_ahead
+    forecasts = {}
+    for i in range(len(ts) - end_loc):
+        sys.stdout.write('-')
+        sys.stdout.flush()
+        res = best_model.fit(first_obs=start_loc, last_obs=i + end_loc, disp='off')
+        temp = res.forecast(horizon=1, start=end_loc).variance
+        fcast = temp.iloc[i]
+        forecasts[fcast.name] = fcast
+    print(' Done!')
+    variance_holdout = pd.DataFrame(forecasts).T
+    variance_holdout = pd.Series(variance_holdout['h.1'])
+    #print(best_results.summary())
 
-# Print top 5 rows of variance forecast with an expanding window
-print(variance_expandwin.head(15))
-# Print top 5 rows of variance forecast with a fixed rolling window
-print(variance_fixedwin.head(15))
-
-##Simplify the model with p-values
-
-# Print model fitting summary
-print(best_results.summary())
-
-
-
-# Print out parameter stats
-print(para_summary)
-
-
-
-
-
-## Backtesting with MAE, MSE
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-def evaluate(observation, forecast): 
-    # Call sklearn function to calculate MAE
-    mae = mean_absolute_error(observation, forecast)
-    print('Mean Absolute Error (MAE): {:.3g}'.format(mae))
-    # Call sklearn function to calculate MSE
-    mse = mean_squared_error(observation, forecast)
-    print('Mean Squared Error (MSE): {:.3g}'.format(mse))
-    return mae, mse
 
 # Backtest model with MAE, MSE
-evaluate(raw['return'].sub(raw['return'].mean()).pow(2), bm_std**2)
+observed_var = raw['return'].tail(forecast_ahead).sub(raw['return'].tail(forecast_ahead).mean()).pow(2)
+evaluate(observed_var, variance_holdout)
 
+'''
 # Plot the actual  volatility
+bm_std = best_results.conditional_volatility
 plt.plot(raw['return'].sub(raw['return'].mean()).pow(2),
          color = 'grey', alpha = 0.4, label = 'Daily Volatility')
 
@@ -171,3 +159,11 @@ plt.plot(raw['return'].sub(raw['return'].mean()).pow(2),
 plt.plot(bm_std**2, color = 'red', label = 'Best Model Volatility')
 plt.legend(loc = 'upper right')
 plt.show()
+'''
+
+if best_vol == 'EGARCH':
+    forecast = best_results.forecast(horizon=forecast_ahead, method="bootstrap").variance.T
+else:
+    forecast = best_results.forecast(horizon=forecast_ahead).variance.T
+
+conf = np.sqrt(forecast)*1.96
