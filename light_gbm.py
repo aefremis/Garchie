f826a730f2606e1d33bac3707dd8c72f25bcf3b4
aefremis <aefremis@hotmail.com>
2023-12-06 18:04:48 +0200

    light gbm initial part

---
+++ light_gbm.py
@@ -0,0 +1,29 @@
+from fetch_equity import crypto, asset
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import lightgbm as gbm
+
+# select asset
+asset_series = asset(symbol='vusa.as', granularity='1d', start='2023-01-01', end='2023-11-21')
+print(asset_series)
+raw = asset_series.fetch_asset()
+raw['typical_price'] = np.round((raw['high'] + raw['low'] + raw['close']) / 3,2)
+ts = raw[['date', 'typical_price']].copy()
+
+# build covariates based on granularity / now only daily for prototype
+new_cols = ['month', 'week', 'day', 'dayofweek', 'quarter']
+for i in new_cols:
+    ts[f'{i}'] = eval('ts["date"].dt.'+i)
+
+ts['wom'] = ts["date"].apply(lambda d: (d.day-1) // 7 + 1)
+ts.set_index('date', inplace=True)
+
+# decide on lag number via acf
+acf_values = sm.tsa.stattools.acf(ts['typical_price'], nlags=len(ts)-1)
+acf_val = acf_values[1:]
+significant_count = sum(acf > 0.90 for acf in acf_val)
+
+# build lag covariates
+for i in range(1,significant_count+1):
+    ts[f'lag_{i}'] = ts['typical_price'].shift(i)
