b9d6fe51d43832eb8a29c98927a139e2409235a6
aefremis <aefremis@hotmail.com>
2024-01-24 13:04:18 +0200

    improvement in cv of lightgbm

--- light_gbm.py
+++ light_gbm.py
@@ -4,18 +4,19 @@ import numpy as np
 import statsmodels.api as sm
 import lightgbm as gbm
 from lightgbm import LGBMRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
 
 
 # select asset
-asset_series = asset(symbol='IBM', granularity='1d', start='2023-01-01', end='2023-12-10')
+asset_series = asset(symbol='IBM', granularity='1d', start='2020-04-09', end='2024-01-05')
 print(asset_series)
 raw = asset_series.fetch_asset()
 raw['typical_price'] = np.round((raw['high'] + raw['low'] + raw['close']) / 3,2)
 ts = raw[['date', 'typical_price']].copy()
 
 # build covariates based on granularity / now only daily for prototype
-new_cols = ['month', 'week', 'day', 'dayofweek', 'quarter']
+new_cols = ['quarter', 'month', 'week', 'day', 'dayofweek']
 for i in new_cols:
     ts[f'{i}'] = eval('ts["date"].dt.'+i)
 
@@ -25,7 +26,7 @@ ts.set_index('date', inplace=True)
 # decide on lag number via acf
 acf_values = sm.tsa.stattools.acf(ts['typical_price'], nlags=len(ts)-1)
 acf_val = acf_values[1:]
-significant_count = sum(acf > 0.90 for acf in acf_val)
+significant_count = sum(acf > 0.95 for acf in acf_val)
 
 # build lag covariates
 for i in range(1,significant_count+1):
@@ -35,7 +36,7 @@ for i in range(1,significant_count+1):
 ts.dropna(axis='rows', inplace=True)
 
 # train test split
-hold_out = 0.95
+hold_out = 0.99
 if  hold_out != 0.8:
     train_size = hold_out
 else:
@@ -45,26 +46,29 @@ split_idx = round(len(ts) * train_size)
 
 # Split
 train, test = ts.iloc[:split_idx], ts.iloc[split_idx:]
+cv_split = TimeSeriesSplit(n_splits=4, test_size=100)
 
 # hyperparameters tuning - cv
 fixed_params = {
     'objective': 'regression',
     'metric': 'rmse',
-    'bagging_freq': 5,
+    'bagging_freq': 10,
     'boosting_type': 'gbdt',
     'num_rounds': 100,
 }
 
 search_params = {
+    'max_depth': [3, 4, 6, 5, 10],
     'num_leaves': [12, 20, 32],
     'learning_rate': [0.01, 0.05, 0.1],
-    'feature_fraction': [0.7, 0.8, 0.9],
+    'n_estimators': [100, 200],
+    'feature_fraction': [0.8, 0.9],
 }
 
 base_model = LGBMRegressor(**fixed_params)
 grid_search = GridSearchCV(estimator=base_model,
                            param_grid=search_params,
-                           cv=5,
+                           cv=cv_split,
                            scoring="neg_root_mean_squared_error",
                            n_jobs=-1,
                            verbose=False)
@@ -76,7 +80,14 @@ print("Best Parameters: ", grid_search.best_params_)
 
 best_model = LGBMRegressor(**fixed_params, **grid_search.best_params_)
 best_model.fit(X,y)
-best_model.predict(test.loc[:, train.columns != 'typical_price'])
-test['typical_price']
+prediction = best_model.predict(test.loc[:, train.columns != 'typical_price'])
+y_test = test['typical_price']
 
-#fix it
+def evaluate_model(y_test, prediction):
+  print(f"MAE: {mean_absolute_error(y_test, prediction)}")
+  print(f"MSE: {mean_squared_error(y_test, prediction)}")
+  print(f"MAPE: {mean_absolute_percentage_error(y_test, prediction)}")
+
+evaluate_model(y_test,prediction)
+
+#to forecast ahead - build lagged process
